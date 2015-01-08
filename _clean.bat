5c405be83d2b798fc23e71d3cdd57f4f5021db2d
ikeblaster <ike@thez.info>
2015-01-08 23:21:37 +0100

    Clean .bat

---
+++ _clean.bat
@@ -0,0 +1,17 @@
+cd cppcli_cpp_flatapi
+call _clean.bat
+
+cd ..\cs_cpp_clibridge
+call _clean.bat
+
+cd ..\cs_cpp_clrhosting
+call _clean.bat
+
+cd ..\cs_cpp_com
+call _clean.bat
+
+cd ..\cs_cpp_mixedmode
+call _clean.bat
+
+cd ..\cs_cpp_monoembed\windows
+call _clean.bat
