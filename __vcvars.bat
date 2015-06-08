f0c67cc844c80cff7f70ffc1c7027ac9d5e3e9a6
ikeblaster <ike@thez.info>
2015-06-09 00:55:51 +0200

    Updated __vcvars.bat

--- __vcvars.bat
+++ __vcvars.bat
@@ -1,1 +1,22 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+:: SET THIS PATH TO CORRECT ONE
+
+@set path__vcvarsall="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+
+
+:: DO NOT MODIFY BELOW.
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@if exist %path__vcvarsall% (
+    @call %path__vcvarsall%
+) else (
+    @echo Error!
+    @echo.
+    @echo Please set correct path to vcvarsall.bat in file:
+    @echo %~f0
+    @echo.
+    pause
+    exit
+)
+
